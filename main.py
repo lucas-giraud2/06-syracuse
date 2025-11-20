1ed2eb22c74a0780c4a8a892d84ba548e7ca5453
lucas.giraud <lucasgiraud62@gmail.com>
2025-11-20 09:43:25 +0000

    Fonctions affichant les caractéristique d'une suite de Syracuse

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+"""Programme principal pour la suite de Syracuse
+Contient la fonction principale et les appels aux fonctions secondaires"""
 #### Fonctions secondaires
 
 
@@ -6,6 +8,7 @@ from plotly.graph_objects import Scatter, Figure
 
 ### NE PAS MODIFIER ###
 def syr_plot(lsyr):
+    """Trace la suite de Syracuse"""
     title = "Syracuse" + " (n = " + str(lsyr[0]) + " )"
     fig = Figure({  'layout':   { 'title': {'text': title},
                                 'xaxis': {'title': {'text':"x"}},
@@ -14,26 +17,29 @@ def syr_plot(lsyr):
                 }
     )
 
-    x = [ i for i in range(len(lsyr)) ]
+    x = list(range(len(lsyr)))
     t = Scatter(x=x, y=lsyr, mode="lines+markers", marker_color = "blue")
     fig.add_trace(t)
     fig.show()
     # fig.write_html('fig.html', include_plotlyjs='cdn')
-    return None
+    #return None
 #######################
 
 def syracuse_l(n):
     """retourne la suite de Syracuse de source n
-
     Args:
         n (int): la source de la suite
 
     Returns:
         list: la suite de Syracuse de source n
     """
-
-    # votre code ici 
-    l = [ ]
+    l = [ n ]
+    while n!=1:
+        if n%2==0:
+            n=n//2
+        else:
+            n=3*n+1
+        l.append(n)
     return l
 
 def temps_de_vol(l):
@@ -45,10 +51,7 @@ def temps_de_vol(l):
     Returns:
         int: le temps de vol
     """
-    
-    # votre code ici
-
-    n = 0
+    n = len(l)-1
     return n
 
 def temps_de_vol_en_altitude(l):
@@ -60,10 +63,12 @@ def temps_de_vol_en_altitude(l):
     Returns:
         int: le temps de vol en altitude
     """
-
-    # votre code ici
-
+    k = l[0]
     n = 0
+    for i in range(1, len(l)):
+        if l[i]<k:
+            n=i-1
+            break
     return n
 
 
@@ -76,21 +81,18 @@ def altitude_maximale(l):
     Returns:
         int: l'altitude maximale
     """
-    
-    # votre code ici
-    
-    n = 0
-    return n
+    return max(l)
 
 
 #### Fonction principale
 
 
 def main():
-
+    """Fonction principale du programme"""
     # vos appels à la fonction secondaire ici
-    lsyr = syracuse_l(15)
-    syr_plot(lsyr)
+    lsyr = syracuse_l(3)
+    print(lsyr)
+    #syr_plot(lsyr)
     print(temps_de_vol(lsyr))
     print(temps_de_vol_en_altitude(lsyr))
     print(altitude_maximale(lsyr))
